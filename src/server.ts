2248f7ca240ea6e693da2ae4bec1bd3cbcf76e1f
JMSBRN <zakhavai@gmail.com>
2023-01-28 10:23:19 +0300

    feat: created addUser functional.

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,9 @@ import dotenv from 'dotenv';
 import { setResponseNotFound } from './utils';
 import * as user from './users';
 
-const { getUsers } = user;
+const {
+  getUsers, createUser, updateUser, deleteUser,
+} = user;
 dotenv.config();
 const { PORT } = env;
 const port = PORT || 5000;
@@ -13,6 +15,15 @@ const server = () => {
     switch (req.method) {
       case 'GET':
         getUsers(req, res);
+        break;
+      case 'POST':
+        createUser(req, res);
+        break;
+      case 'PUT':
+        updateUser();
+        break;
+      case 'DELETE':
+        deleteUser();
         break;
       default:
         setResponseNotFound(res);
--- src/users.ts
+++ src/users.ts
@@ -1,17 +1,17 @@
 import { v4 as uuidv4 } from 'uuid';
 import { setResponseNotFound } from './utils';
 import { IUser, ServerListener } from './interfaces';
-import users from './data/users.json';
+import json from './data/users.json';
 
-uuidv4();
-
+const users: IUser[] = json;
 export const getUsers: ServerListener = async (req, res) => {
+  const baseUrl = req.url?.substring(0, req.url.lastIndexOf('/') + 1);
   const id = req.url?.split('/')[3];
   if (req.url === '/api/users') {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.write(JSON.stringify(users));
     res.end();
-  } else if (id) {
+  } else if (baseUrl === '/api/users/' && id) {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     const newArr: IUser[] = [];
     users.forEach((el) => {
@@ -29,6 +29,26 @@ export const getUsers: ServerListener = async (req, res) => {
     setResponseNotFound(res);
   }
 };
-export const createUser = async () => {};
+export const createUser: ServerListener = async (req, res) => {
+  if (req.url === '/api/users') {
+    let body = '';
+    req.on('data', (data) => {
+      body += data.toString();
+    }).on('end', () => {
+      try {
+        const user = JSON.parse(body);
+        user.id = uuidv4();
+        users.push(user);
+        res.writeHead(201, { 'Content-Type': 'application/json' });
+        res.end();
+      } catch (error) {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.write('Bad Post Data.  Is your data a proper JSON?\n');
+        res.end();
+      }
+    });
+  }
+};
+
 export const updateUser = async () => {};
 export const deleteUser = async () => {};
