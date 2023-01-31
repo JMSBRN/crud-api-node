98b006791a6b672511e63978dd65c24023a645db
JMSBRN <zakhavai@gmail.com>
2023-01-31 20:41:08 +0300

    fix: updated updateUser function.

--- src/users.ts
+++ src/users.ts
@@ -1,13 +1,10 @@
-import { v4 as uuidv4 } from 'uuid';
-import { writeFile } from 'fs';
-import { join } from 'path';
-import { cwd } from 'process';
 import { setCheckIsIUser } from './helpers';
 import {
   setResponseWithErrorMessage,
   getUsersFromResponse,
   getUserByIdFromResponse,
   createUserWithResponse,
+  updateUserWIthResponse,
 } from './utils';
 import { IUser, ServerListener } from './interfaces';
 import json from './data/users.json';
@@ -39,15 +36,7 @@ export const createUser: ServerListener = async (req, res) => {
       .on('end', () => {
         try {
           const user = JSON.parse(body);
-          const isUser: boolean = setCheckIsIUser(user);
-          const userWithId = { id: uuidv4(), ...user };
-          if (isUser) {
-            users.push(userWithId);
-            res.writeHead(201, { 'Content-Type': 'application/json' });
-            res.end();
-          } else {
-            setResponseWithErrorMessage(400, res, { title: 'ERROR', message: 'Body does not contain required fields' });
-          }
+          createUserWithResponse(res, users, user);
         } catch (error) {
           setResponseWithErrorMessage(400, res, { title: 'ERROR', message: 'Bad body Data.  Is your data a proper JSON?\n' });
         }
@@ -77,7 +66,15 @@ export const updateUser:ServerListener = async (req, res) => {
         .on('end', () => {
           try {
             const user = JSON.parse(body);
-            createUserWithResponse(res, users, user);
+            const isUser: boolean = setCheckIsIUser(user);
+            if (isUser) {
+              // update user with body request
+              Object.assign(users[users.findIndex((el) => el.id === reqId)], user);
+              res.writeHead(201, { 'Content-Type': 'application/json' });
+              res.end();
+            } else {
+              setResponseWithErrorMessage(400, res, { title: 'ERROR', message: 'Body does not contain required fields' });
+            }
           } catch (error) {
             setResponseWithErrorMessage(400, res, { title: 'ERROR', message: 'Bad body data' });
           }
@@ -95,28 +92,12 @@ export const updateUser:ServerListener = async (req, res) => {
   }
 };
 export const deleteUser: ServerListener = async (req, res) => {
-  const baseUrl = req.url?.substring(0, req.url.lastIndexOf('/') + 1);
   const reqId = req.url?.split('/')[3];
-  if (baseUrl === '/api/users/' && regexp.test(reqId || '')) {
-    const newArr: IUser[] = [];
-    users.forEach((el) => {
-      if (el.id === reqId) {
-        newArr.push(el);
-      }
-    });
-    if (newArr.length) {
-      const newUsers = users.splice(users.findIndex((el) => el.id === reqId), 1);
-      writeFile(join(cwd(), 'src/data/', 'users.json'), JSON.stringify(newUsers), (err) => { if (err) throw err; });
-      res.writeHead(204, { 'Content-Type': 'application/json' });
-      res.end();
-    } else {
-      setResponseWithErrorMessage(404, res, { title: 'ERROR', message: 'User not Found' });
-    }
-  } else {
-    if (!regexp.test(reqId || '')) {
-      setResponseWithErrorMessage(400, res, { title: 'ERROR', message: 'UUID not valid' });
-      return;
-    }
-    setResponseWithErrorMessage(404, res, { title: 'NO FOUND', message: 'Route not found' });
+
+  await updateUserWIthResponse(req, res, users);
+  if (!regexp.test(reqId || '')) {
+    setResponseWithErrorMessage(400, res, { title: 'ERROR', message: 'UUID not valid' });
+    return;
   }
+  setResponseWithErrorMessage(404, res, { title: 'NO FOUND', message: 'Route not found' });
 };
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,9 @@
+import { writeFile } from 'fs';
 import { IncomingMessage, ServerResponse } from 'http';
+import { join } from 'path';
+import { cwd } from 'process';
 import { v4 as uuidv4 } from 'uuid';
+import { regexp } from './constants';
 import { setCheckIsIUser } from './helpers';
 import { IErrorMessage, IUser, ResponseWithErrorMessage } from './interfaces';
 
@@ -46,5 +50,29 @@ export const createUserWithResponse = async (res: ServerResponse, users: IUser[]
     res.end();
   } else {
     setResponseWithErrorMessage(400, res, { title: 'ERROR', message: 'Body does not contain required fields' });
+  }
+};
+export const updateUserWIthResponse = async (
+  req: IncomingMessage,
+  res: ServerResponse,
+  users: IUser[],
+) => {
+  const baseUrl = req.url?.substring(0, req.url.lastIndexOf('/') + 1);
+  const reqId = req.url?.split('/')[3];
+  if (baseUrl === '/api/users/' && regexp.test(reqId || '')) {
+    const newArr: IUser[] = [];
+    users.forEach((el) => {
+      if (el.id === reqId) {
+        newArr.push(el);
+      }
+    });
+    if (newArr.length) {
+      const newUsers = users.splice(users.findIndex((el) => el.id === reqId), 1);
+      writeFile(join(cwd(), 'src/data/', 'users.json'), JSON.stringify(newUsers), (err) => { if (err) throw err; });
+      res.writeHead(204, { 'Content-Type': 'application/json' });
+      res.end();
+    } else {
+      setResponseWithErrorMessage(404, res, { title: 'ERROR', message: 'User not Found' });
+    }
   }
 };
