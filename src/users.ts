45f5296d20c7bd7a83aca3823b969a8317fd8ea0
JMSBRN <zakhavai@gmail.com>
2023-02-02 21:49:11 +0300

    fix: upd. updateUserWithresponse function.

--- src/users.ts
+++ src/users.ts
@@ -1,21 +1,17 @@
-import { setCheckIsIUser } from './helpers';
 import {
   setResponseWithErrorMessage,
   getUsersFromResponse,
   getUserByIdFromResponse,
   createUserWithResponse,
-  updateUserWIthResponse,
+  deleteUserWIthResponse,
   bodyParser,
+  updateUserWithresponse,
 } from './utils';
 import { IUser, ServerListener } from './interfaces';
 import json from './data/users.json';
 import {
   ErrorBadData,
-  ErrorIdNotValid,
-  ErrorNoFields,
-  ErrorNotUser,
   ErrorRouteNotFound,
-  regexp,
   StatusCode,
 } from './constants';
 
@@ -48,49 +44,17 @@ export const createUser: ServerListener = async (req, res) => {
 
 export const updateUser: ServerListener = async (req, res) => {
   const baseUrl = req.url?.substring(0, req.url.lastIndexOf('/') + 1);
-  const reqId = req.url?.split('/')[3];
-  if (baseUrl === '/api/users/' && regexp.test(reqId || '')) {
-    const newArr: IUser[] = [];
-    res.writeHead(StatusCode.SUCCESS, { 'Content-Type': 'application/json' });
-    users.forEach((el) => {
-      if (el.id === reqId) {
-        newArr.push(el);
-      }
-    });
-    if (newArr.length) {
-      try {
-        const user = await bodyParser(req, res);
-        const isUser: boolean = setCheckIsIUser(user);
-        if (isUser) {
-          // update user with body request
-          Object.assign(users[users.findIndex((el) => el.id === reqId)], user);
-          res.writeHead(StatusCode.CREATED, { 'Content-Type': 'application/json' });
-          res.end();
-        } else {
-          setResponseWithErrorMessage(StatusCode.BAD_REQUEST, res, ErrorNoFields);
-        }
-      } catch (error) {
-        setResponseWithErrorMessage(StatusCode.BAD_REQUEST, res, ErrorBadData);
-      }
-    } else {
-      setResponseWithErrorMessage(StatusCode.NOT_FOUND, res, ErrorNotUser);
-    }
-    res.end();
-  } else {
-    if (!regexp.test(reqId || '')) {
-      setResponseWithErrorMessage(StatusCode.BAD_REQUEST, res, ErrorIdNotValid);
-      return;
-    }
-    setResponseWithErrorMessage(StatusCode.NOT_FOUND, res, ErrorRouteNotFound);
+  const id = req.url?.split('/')[3];
+  if (baseUrl === '/api/users/' && id) {
+    await updateUserWithresponse(req, res, users);
   }
+  setResponseWithErrorMessage(StatusCode.NOT_FOUND, res, ErrorRouteNotFound);
 };
 export const deleteUser: ServerListener = async (req, res) => {
-  const reqId = req.url?.split('/')[3];
-
-  await updateUserWIthResponse(req, res, users);
-  if (!regexp.test(reqId || '')) {
-    setResponseWithErrorMessage(StatusCode.BAD_REQUEST, res, ErrorIdNotValid);
-    return;
+  const baseUrl = req.url?.substring(0, req.url.lastIndexOf('/') + 1);
+  const id = req.url?.split('/')[3];
+  if (baseUrl === '/api/users/' && id) {
+    await deleteUserWIthResponse(req, res, users);
   }
   setResponseWithErrorMessage(StatusCode.NOT_FOUND, res, ErrorRouteNotFound);
 };
