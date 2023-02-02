60e496bea33b6e9fba0f76e84e6a966c94351b97
JMSBRN <zakhavai@gmail.com>
2023-02-02 21:47:54 +0300

    fix: upd. updateUserWithresponse function.

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-import { IError } from './interfaces';
+import { IError, IUser } from './interfaces';
 
 export const regexp: RegExp = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/i;
 export const ErrorNotUser: IError = { title: 'ERROR', message: 'User not Found' };
@@ -9,7 +9,8 @@ export const ErrorNoFields: IError = {
 };
 export const ErrorBadData: IError = { title: 'ERROR', message: 'Bad body data' };
 export const ErrorIdNotValid: IError = { title: 'ERROR', message: 'UUID not valid' };
-export const ErrorRouteNotFound: IError = { title: 'ERROR', message: 'Route not Found' };
+export const ErrorRouteNotFound: IError = { title: 'ERROR', message: 'Oops! Route not Found' };
+export const ErrorNoRequiredFields: IError = { title: 'ERROR', message: 'does not contain required fields' };
 
 export enum StatusCode {
   'NOT_FOUND' = 404,
@@ -18,3 +19,11 @@ export enum StatusCode {
   'CREATED' = 201,
   'NOT_CONTENT' = 204,
 }
+export const testUser: IUser = {
+  id: 'test',
+  username: 'test',
+  age: 18,
+  hobbies: [
+    'test',
+  ],
+};
