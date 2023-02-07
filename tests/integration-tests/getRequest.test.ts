cc30fb00ef4206e34d944ee5bda754a4185e9116
JMSBRN <zakhavai@gmail.com>
2023-02-07 13:06:58 +0300

    fix: updated test for get requests.

--- tests/integration-tests/getRequest.test.ts
+++ tests/integration-tests/getRequest.test.ts
@@ -3,12 +3,12 @@ import { promisify } from "util";
 import server from "../../src";
 import { testUser } from "../mocks/testUser";
 
-describe('GET request',() => {
+describe('testing GET requests are woking correctly ',() => {
     const r = supertest(server);
     afterAll(async () => {
         await promisify(server.close.bind(server))();
        });
-    it('GET get users with empty array', async ()=> {
+    it('should GET get users with empty array', async ()=> {
         await r
              .get('/api/users')
              .set('Accept', 'application/json')
@@ -19,7 +19,7 @@ describe('GET request',() => {
              })
              
        });
-    it('GET get users with creted user', async ()=> {
+    it(' should GET get users with creted user', async ()=> {
         await r
         .post('/api/users')
            .send(JSON.stringify(testUser))
@@ -31,17 +31,16 @@ describe('GET request',() => {
              .expect(200)
              .then((r) => {
               expect(r.body).toHaveLength(1);
-              expect(r.body[0]).toHaveProperty('id');
+              expect(r.body[0].id).toHaveLength(36);
               expect(r.body[0]).toHaveProperty('username', 'test');
               expect(r.body[0]).toHaveProperty('age', 18);
               expect(r.body[0]).toHaveProperty('hobbies', [
                 'test'
               ]);
-              expect(r.body[0].id).toHaveLength(36);
              })
              
        });
-       it('GET with not correct route', async () => {
+       it(' should GET with not correct route', async () => {
           await  r.get('/api/user')
             .set('Accept', 'application/json')
              .expect('Content-Type', /json/)
@@ -50,5 +49,32 @@ describe('GET request',() => {
                 expect(r.body).toHaveProperty('message', 'Oops! Route not Found');
              })
        })
-      
+       it('should GET get user by id  with not valid id', async () => {
+          await  r.get('/api/users/test')
+            .set('Accept', 'application/json')
+             .expect('Content-Type', /json/)
+             .expect(400)
+             .then((r) => {
+                expect(r.body).toHaveProperty('message', 'UUID not valid');
+             })
+       })
+       it('should GET get user by id  with not exist user', async () => {
+          await  r.get('/api/users/eb0cd1b8-1c0d-4bca-a201-310df50a4130')
+            .set('Accept', 'application/json')
+             .expect('Content-Type', /json/)
+             .expect(404)
+             .then((r) => {
+                expect(r.body).toHaveProperty('message', 'User not Found');
+             })
+       })
+       it('should GET get user by id  with not correct route', async () => {
+          await  r.get('/api/test/users/eb0cd1b8-1c0d-4bca-a201-310df50a4130')
+            .set('Accept', 'application/json')
+             .expect('Content-Type', /json/)
+             .expect(404)
+             .then((r) => {
+                expect(r.body).toHaveProperty('message', 'Oops! Route not Found');
+             })
+       })
 });
+ 
