c8d7d29ef755745fd45c2782d024dc1664a6eddb
JMSBRN <zakhavai@gmail.com>
2023-03-04 10:30:51 +0300

    feat: created gracefull-kill cluster functional

--- src/cluster/cluster.ts
+++ src/cluster/cluster.ts
@@ -6,6 +6,7 @@ import server from '../server';
 import { checkArgValue, stdoutWrite } from '../helpers';
 import createLoadBalancer from './loadBalancer.';
 import createWorker from './worker';
+import { gracefulKill } from './graceful-kill';
 
 dotenv.config();
 const { PORT } = env;
@@ -13,16 +14,18 @@ const port = +PORT! || 5000;
 const numCPUs = os.cpus().length;
 
 export const clusterApp = () => {
+  process.on('SIGINT', () => {
+    stdoutWrite('Received SIGINT signal, gracefully stopping the cluster process');
+    gracefulKill();
+  });
   const clusterArgv = checkArgValue(2, '--cluster');
   if (clusterArgv) {
     if (cluster.isPrimary) {
       const workers = [] as Worker[];
-
       // Fork workers
       for (let i = 0; i < numCPUs; i += 1) {
         const worker = cluster.fork({ PORT_WORKER: port + i + 1 });
         workers.push(worker);
-
         // Restart worker on exit
         worker.on('exit', () => {
           stdoutWrite(`Worker ${worker.process.pid} died. Restarting...`);
---
+++ src/cluster/graceful-kill.ts
@@ -0,0 +1,33 @@
+import cluster, { Worker } from 'cluster';
+import { stdoutWrite } from '../helpers';
+
+export function gracefulKill(timeout = 5000): void {
+  stdoutWrite('Gracefully stopping the cluster process');
+  if (cluster.workers) {
+    Object.values(cluster.workers).forEach((worker: Worker | undefined) => {
+    // Gracefully disconnect the worker process
+      if (worker) {
+        worker.disconnect();
+        worker.on('disconnect', () => {
+          stdoutWrite(`Worker ${worker.id} has disconnected`);
+        });
+      }
+    });
+
+    const timer = setTimeout(() => {
+      stdoutWrite(`Worker processes did not disconnect within ${timeout} ms, forcefully stopping the cluster process`);
+      process.exit(1);
+    }, timeout);
+
+    // Listen for the 'exit' event to know when all worker processes have exited
+    cluster.on('exit', () => {
+      if (Object.keys(cluster.workers!).length === 0) {
+        stdoutWrite('/n All worker processes have exited, stopping the master process');
+        clearTimeout(timer);
+        process.exit();
+      }
+    });
+  }
+}
+
+export default gracefulKill;
